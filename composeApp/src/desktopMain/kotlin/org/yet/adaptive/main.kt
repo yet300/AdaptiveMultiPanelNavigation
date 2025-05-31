23e4c632c32db7fbfe96ed7d3f38edb16f4a6304
yet <ghg00rus300@gmail.com>
2025-05-31 15:38:27 +0400

    Feat: Initialize Decompose root component on desktop
    This commit initializes the Decompose `DefaultRootComponent` for the desktop application.
    
    Key changes:
    - Created a `LifecycleRegistry` for Decompose.
    - Instantiated `DefaultRootComponent` on the UI thread using `runOnUiThread`.
    - Configured the main `Window` with a `rememberWindowState` and set a minimum size.
    - Passed the `root` component to the `App` composable.
    - Added a `runOnUiThread` utility function to execute blocks of code on the Swing event dispatch thread.

--- composeApp/src/desktopMain/kotlin/org/yet/adaptive/main.kt
+++ composeApp/src/desktopMain/kotlin/org/yet/adaptive/main.kt
@@ -1,13 +1,60 @@
 package org.yet.adaptive
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.app.root.DefaultRootComponent
+import java.awt.Dimension
+import javax.swing.SwingUtilities
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 
 fun main() = application {
+    val lifecycle = LifecycleRegistry()
+
+    val root =
+        runOnUiThread {
+            DefaultRootComponent(
+                componentContext = DefaultComponentContext(lifecycle = lifecycle),
+            )
+        }
+
+    val windowState = rememberWindowState(
+        size = DpSize(800.dp, 600.dp)
+    )
+
     Window(
+        state = windowState,
         onCloseRequest = ::exitApplication,
         title = "AdaptiveMultiPanelNavigation",
     ) {
-        App()
+        window.minimumSize = Dimension(400.dp.value.toInt(), 600.dp.value.toInt())
+
+        App(root)
     }
+}
+
+
+private fun <T> runOnUiThread(block: () -> T): T {
+    if (SwingUtilities.isEventDispatchThread()) {
+        return block()
+    }
+
+    var error: Throwable? = null
+    var result: T? = null
+
+    SwingUtilities.invokeAndWait {
+        try {
+            result = block()
+        } catch (e: Throwable) {
+            error = e
+        }
+    }
+
+    error?.also { throw it }
+
+    @Suppress("UNCHECKED_CAST")
+    return result as T
 }
